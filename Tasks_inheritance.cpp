36d41ebdb4c1552292504a8d7ba8efe9a82cdd3b
VKONSTANTINIUS <kosrounder@gmail.com>
2025-08-10 12:24:18 +0300

    Update Tasks_inheritance.cpp

--- Tasks_inheritance.cpp
+++ Tasks_inheritance.cpp
@@ -1,4 +1,10 @@
-﻿//За допомогою механізму успадкування, реалізуйте клас Aspirant, 
+#include <iostream>
+#include <string>
+#include <windows.h> 
+
+using namespace std;
+
+//За допомогою механізму успадкування, реалізуйте клас Aspirant, 
 //похідний від Student(аспірант - це студент, який готується 
 //до захисту кандидатської роботи).
 //Тема кандидатської роботи - звичайне поле типу string.
@@ -6,7 +12,80 @@
 //і всіх необхідних геттерів / сеттерів у кожному з двох класів.
 //Застосувати виклик конструктора базового класу, 
 //а також для виклику 1 - 2 інших методів(наприклад, Print).
-//
+
+class Student {
+
+private:
+
+    string name;
+    string surname;
+    string faculty;
+    string group;
+
+public:
+
+    Student(): Student("Тарас", "Шевченко", "Розробка ПЗ", "СПР411"){}
+    
+    Student(
+        string name, 
+        string surname, 
+        string faculty, 
+        string group) {
+
+        setName(name);
+        setSurname(surname);
+        setFaculty(faculty);
+        setGroup(group);  
+
+    }
+
+    void setName    (string name)    { this->name = name; }
+    void setSurname (string surname) { this->surname = surname; }
+    void setFaculty (string faculty) { this->faculty = faculty; }
+    void setGroup   (string group)   { this->group = group; }
+
+    string getName()    const { return name; }
+    string getSurname() const { return surname; }
+    string getFaculty() const { return faculty; }
+    string getGroup()   const { return group; }
+
+   
+    void Print () const {
+        cout << "Ім'я, Прізвище - " << getName() << " " << getSurname() << endl;
+        cout << "Факультет      - " << getFaculty() << endl;
+        cout << "Група          - " << getGroup() << endl;   
+    }
+
+};
+
+class Aspirant : public Student {
+
+private:
+
+    string science_work;
+
+public:
+
+    Aspirant() { setScience_work("Computer vision in defence"); }
+
+    Aspirant(string name, string surname, string faculty, string group, string science_work ): 
+        Student(name, surname, faculty, group) { 
+
+        setScience_work(science_work);
+    }
+
+    void setScience_work(string science_work) { this->science_work = science_work; }
+
+    string getScience_work() const { return science_work; }
+
+    void Print() const {
+        Student::Print();
+        cout << "Тема кандидатської роботи: " << getScience_work() << endl;
+    }
+};
+
+
+
 //Створіть клас Passport(паспорт), який міститиме паспортну інформацію про громадянина України.
 //За допомогою механізму успадкування, реалізуйте клас ForeignPassport(Закордонний паспорт), 
 //похідний від Passport.Загран.паспорт містить крім паспортних даних, також дані про візи.
@@ -60,10 +139,23 @@
 //- На основі деяких за допомоги множинного успадкування із цих типів спробуйте створити 
 //- клас Button, для відображення кнопки (на кнопці має бути текст)!
 
-#include <iostream>
+
 
 int main()
 {
-    
+    SetConsoleOutputCP(1251);
+
+    Student Sevchenko;
+    Sevchenko.Print();
+    cout << "\n";
+
+    Aspirant Franko("Іван", 
+                    "Франко", 
+                    "Гуманітарні науки", 
+                    "Гр-2025", 
+                    "Самовизнання штучного інтелекту");
+
+    Franko.Print();
+    cout << "\n";
 }
 
